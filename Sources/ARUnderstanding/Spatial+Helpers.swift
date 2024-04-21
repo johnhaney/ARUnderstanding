b903fcd233da9c6565b836a6b16fc2f08e74755a
John Haney <5025692+johnhaney@users.noreply.github.com>
2024-04-21 16:44:24 -0500

    Spatial Helpers for Pose3D, Rotation3D, Point3D, Vector3D

---
+++ Sources/ARUnderstanding/Spatial+Helpers.swift
@@ -0,0 +1,89 @@
+//
+//  Spatial+Helpers.swift
+//
+//
+//  Created by John Haney on 4/20/24.
+//
+
+import Spatial
+import RealityKit
+
+extension Pose3D {
+    /// Normalized vector in the direction of the pose's rotation
+    var forward: Vector3D {
+        rotation.forward
+    }
+    var up: Vector3D {
+        rotation.up
+    }
+    var right: Vector3D {
+        rotation.right
+    }
+}
+
+extension Rotation3D {
+    /// Normalized vector in the direction of the pose's rotation
+    var forward: Vector3D {
+        self.act(Vector3D.forward)
+    }
+    
+    var up: Vector3D {
+        self.act(Vector3D.up)
+    }
+    
+    var right: Vector3D {
+        self.act(Vector3D.right)
+    }
+    
+    func act(_ vector: Vector3D) -> Vector3D {
+        Vector3D(quaternion.act(vector.vector))
+    }
+}
+
+extension Pose3D {
+    init?(_ entity: Entity) {
+        self.init(entity.transform)
+    }
+    
+    init?(_ transform: Transform) {
+        self.init(transform.matrix)
+    }
+    
+    var transform: Transform {
+        Transform(rotation: rotation.floatQuaternion, translation: position.floatVector)
+    }
+}
+
+extension Point3D {
+    init(_ entity: Entity) {
+        self.init(entity.transform)
+    }
+    
+    init(_ transform: Transform) {
+        self.init(transform.translation)
+    }
+    
+    var floatVector: SIMD3<Float> {
+        SIMD3<Float>(self)
+    }
+}
+
+extension Rotation3D {
+    init(_ entity: Entity) {
+        self.init(entity.transform)
+    }
+    
+    init(_ transform: Transform) {
+        self.init(transform.rotation)
+    }
+    
+    var floatQuaternion: simd_quatf {
+        simd_quatf(self)
+    }
+}
+
+extension Vector3D {
+    var floatVector: SIMD3<Float> {
+        SIMD3<Float>(self)
+    }
+}
---
+++ Tests/ARUnderstandingTests/SpatialHelperTests.swift
@@ -0,0 +1,44 @@
+//
+//  SpatialHelperTests.swift
+//
+//
+//  Created by John Haney on 4/20/24.
+//
+
+import XCTest
+@testable import ARUnderstanding
+import Spatial
+
+final class SpatialHelperTests: XCTestCase {
+    func testPoseHelpers() {
+        let pose = Pose3D(position: Point3D(x: 1, y: 1, z: 1), rotation: Rotation3D(position: Point3D(x: 1, y: 0, z: 0), target: Point3D(x: 2, y: 0, z: 0), up: .up))
+        
+        XCTAssertEqual(pose.forward.x, 1, accuracy: 0.0001)
+        XCTAssertEqual(pose.forward.y, 0, accuracy: 0.0001)
+        XCTAssertEqual(pose.forward.z, 0, accuracy: 0.0001)
+        
+        XCTAssertEqual(pose.up.x, 0, accuracy: 0.0001)
+        XCTAssertEqual(pose.up.y, 1, accuracy: 0.0001)
+        XCTAssertEqual(pose.up.z, 0, accuracy: 0.0001)
+        
+        XCTAssertEqual(pose.right.x, 0, accuracy: 0.0001)
+        XCTAssertEqual(pose.right.y, 0, accuracy: 0.0001)
+        XCTAssertEqual(pose.right.z, -1, accuracy: 0.0001)
+    }
+    
+    func testRotationHelpers() {
+        let rotation = Rotation3D(angle: .degrees(45), axis: .xyz)
+        
+        XCTAssertEqual(rotation.forward.x, 0.505, accuracy: 0.001)
+        XCTAssertEqual(rotation.forward.y, -0.3106, accuracy: 0.001)
+        XCTAssertEqual(rotation.forward.z, 0.8047, accuracy: 0.001)
+        
+        XCTAssertEqual(rotation.up.x, -0.3106, accuracy: 0.001)
+        XCTAssertEqual(rotation.up.y, 0.8047, accuracy: 0.001)
+        XCTAssertEqual(rotation.up.z, 0.505, accuracy: 0.001)
+        
+        XCTAssertEqual(rotation.right.x, 0.8047, accuracy: 0.001)
+        XCTAssertEqual(rotation.right.y, 0.505, accuracy: 0.001)
+        XCTAssertEqual(rotation.right.z, -0.3106, accuracy: 0.001)
+    }
+}
